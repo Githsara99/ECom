5786ee2ea4a85dee2eaa99b1e95f0d05b0d62ecb
Githsara Senarathne <88274577+Githsara99@users.noreply.github.com>
2026-01-28 14:54:24 +0530

    created mongodb connection

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -1,1 +1,30 @@
-console.log('Hello World');
+// packages
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+// utiles
+//import connectDB from '../config/db.js';
+import { connectDB } from '../config/db.js';
+
+
+// load env vars
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
+export default connectDB;
